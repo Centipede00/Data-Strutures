18a1fab78c16152a2ec635c395960d5d10b1df43
Abhisek samantaray <abhisekcsce@gmail.com>
2020-09-12 19:43:18 +0530

    Updating delete file of Single Linked List

--- Linked List/C/delete.c
+++ Linked List/C/delete.c
@@ -4,31 +4,33 @@ struct NODE{
     int info;
     struct NODE *next;
 };
-void searchDelete(struct NODE *head,int data){
-    struct NODE *p;
-    p = head;
-    int k=1;
-    int found = 0;
-    while(p->next!=NULL){
-        if(p->info==data){
+void delete(struct NODE *,int);
+void searchDelete(struct NODE *,int);
+void create(struct NODE *,int);
+void display(struct NODE *);
+struct NODE *head;
+void searchDelete(struct NODE *node,int data){
+    int found =0,k=1;
+    while(node!=NULL){
+        if(node->info==data){
             delete(head,k);
+            found=1;
             break;
         }
-        p = p->next;
+        node = node->next;
         k++;
     }
-    if(p->info == data)
-        delete(head,k);
-    else
-        printf("Given data is not present inside the Linked List\n");
+    if(found == 0)
+        printf("%d is not present inside Linked List\n",data);
 }
-void delete(struct NODE *head,int position){
+void delete(struct NODE *node,int position){
     struct NODE *p,*q;
     p = head;
     int k=1;
     if(position==1){
-        head = p->next;
+        node = p->next;
         p->next = NULL;
+        head = node;
         free(p);
     }
     else{
@@ -46,7 +48,6 @@ void delete(struct NODE *head,int position){
             free(p);
         }
     }
-    display(head);
 }
 void create(struct NODE *node,int nodes){
     for(int i=0;i<nodes;i++){
@@ -66,16 +67,15 @@ void display(struct NODE *node){
     int i=1;
     printf("\nDetails of current Linked List is:\n");
     printf("Data present inside Nodes:\n");
-    while(node->next!=NULL){
+    while(node!=NULL){
         printf("Node %d: %d\n",i,node->info);
         node = node->next;
         i++;
     }
-    printf("Node %d: %d\n",i,node->info);
-    printf("Number of nodes present:%d\n",i);
+    printf("Number of nodes present:%d\n",i-1);
 }
 int main(){
-    struct NODE *head,*node;
+    struct NODE *node;
     int nodes,option,position,data;
     printf("How many nodes do you want to create?:");
     scanf("%d",&nodes);
@@ -83,33 +83,39 @@ int main(){
     node->next = NULL;
     head = node;
     create(node,nodes);
-    display(head);
-    printf("\n\tOperations Available:\n");
-        printf("1.deletion at the beginning of the List\n");
-        printf("2.deletion at the End of the List\n");
-        printf("3.deletion at a particular position\n");
-        printf("4.deletion of a given node\n");
+    while (option!=5){
+        printf("\n\tOperations Available:\n");
+        printf("1.Deletion at the beginning of the List\n");
+        printf("2.Deletion at the End of the List\n");
+        printf("3.Deletion at a particular position\n");
+        printf("4.Deletion after a given node\n");
+        printf("5.Quit\n");
         printf("Enter any option(1-4):");
         scanf("%d",&option);
-    switch (option){
-        case 1: delete(node,1);
-            break;
-
-        case 2: delete(node,nodes);
-            break;
-        
-        case 3:printf("Enter the position:");
-                scanf("%d",&position);
-                delete(node,position);
-           break;
-        
-        case 4:printf("Enter the data of node,after which will be deleted:");
-                scanf("%d",&data);
-                searchDelete(node,data);
-            break;
-        default:printf("Invalid option\n");
+        if(option == 1){
+            delete(node,1);
+        }
+        else if(option ==2){
+            delete(node,nodes+1);
+        }
+        else if(option==3){
+            printf("Enter the position to delete:");
+            scanf("%d",&position);
+            delete(node,position);
+        }
+        else if(option==4){
+            printf("Enter the data of node,which will be deleted:");
+            scanf("%d",&data);
+            searchDelete(node,data);
+        }
+        else if(option == 5){
             break;
         }
+        else{
+            printf("Invalid Option\n");
+        }
         display(head);
+        system("pause");
+    }
     return 0;
 }
